f4a1c1225404dd01b02f0cfe14643d139584e2d3
MMJimenez <munozmiguel321@gmail.com>
2023-05-08 22:44:01 +0200

    :sparkles: check if the language is avaliable

--- app/src/main/java/com/example/blueparrot/TranslateFragment.kt
+++ app/src/main/java/com/example/blueparrot/TranslateFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import com.google.mlkit.common.model.DownloadConditions
 import com.google.mlkit.nl.translate.TranslateLanguage
 import com.google.mlkit.nl.translate.Translation
 import com.google.mlkit.nl.translate.Translator
@@ -26,6 +27,7 @@ class TranslateFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // TODO: chaged the initialization of the transalate obj. It cant be hardcoded
         val options = TranslatorOptions.Builder()
             .setSourceLanguage(TranslateLanguage.SPANISH)
             .setTargetLanguage(TranslateLanguage.ENGLISH)
@@ -40,7 +42,7 @@ class TranslateFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_translate, container, false)
 
-        edSource = view.findViewById(R.id.ed_origin)
+        edSource = view.findViewById(R.id.ed_source)
         edTarget = view.findViewById(R.id.ed_result)
         btnTranslate = view.findViewById(R.id.btn_translate)
 
@@ -51,16 +53,39 @@ class TranslateFragment : Fragment() {
 
     private fun translate() {
         if (edSource.text.toString().equals("")) return
+        if (!isAvaliableThe(translator)) return
         translator.translate(edSource.text.toString())
             .addOnSuccessListener { translatedText ->
-                Log.v(TAG, "Texto Source: " + edSource.text.toString() + "Texto Target: " + translatedText)
+                Log.v(
+                    TAG,
+                    "Texto Source: " + edSource.text.toString() + "Texto Target: " + translatedText
+                )
                 edTarget.setText(translatedText)
             }
             .addOnFailureListener { exception ->
-                Log.e(TAG, exception.toString())
+                Log.e(TAG, "Failed to translate(): $exception")
             }
     }
 
-
-
+    private fun isAvaliableThe(languageModel: Translator): Boolean {
+        var avaliable = false
+        var conditions = DownloadConditions.Builder() // TODO: Conditions can be in another method...
+            .requireWifi()
+            .build()
+        languageModel.downloadModelIfNeeded(conditions)
+            .addOnSuccessListener {
+                // Model downloaded successfully. Okay to start translating.
+                // (Set a flag, unhide the translation UI, etc.)
+                Log.v(TAG, "Languages Downloaded")
+                avaliable = true
+            }
+            .addOnFailureListener { exception ->
+                // Model couldn’t be downloaded or other internal error.
+                // ...
+                Log.e(TAG, "Languages not donwloaded: $exception")
+                avaliable = false
+            }
+        Log.v(TAG, "is avaliable the language model?: $avaliable")
+        return avaliable
+    }
 }
